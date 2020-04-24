b5057d9ec983ec449476c4060e2dcd06c2c713c7
Guillermo de la Iglesia <zguillez@gmail.com>
2020-04-25 00:12:38 +0200

    update

--- example/index.js
+++ example/index.js
@@ -1,9 +1,7 @@
 const ztoolz = require('../src/Ztoolz');
-
 ztoolz.version();
 // ztoolz.version('minor');
 // ztoolz.version('major');
-
 /* ztoolz.database('SELECT * FROM web_registro', false).then(() => {
   console.log(`done!`.magenta);
   ztoolz.database(['SELECT * FROM web_registro', 'SELECT * FROM web_registro'], true).then(() => {
